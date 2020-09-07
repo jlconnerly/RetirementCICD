93f240d6d34e5bb96fcb55c58de5db8c1f7ee36a
Jake <jlconnerly@gmail.com>
2020-09-07 14:42:03 -0500

    added appCenter crash. PreArchive to fetch debugSymbols

--- RetirementCalculatorCICD/ViewController.swift
+++ RetirementCalculatorCICD/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var plannedRetAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var currentSavingsTextField: UITextField!
+    @IBOutlet weak var valueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if MSCrashes.hasCrashedInLastSession() {
+            let alert = UIAlertController(title: "Oops", message: "Sorry about that, an error has occured.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's OK... I guess...", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
-
+    @IBAction func calcRetButtonTapped(_ sender: UIButton) {
+        MSCrashes.generateTestCrash()
+    }
+    
 }
 
